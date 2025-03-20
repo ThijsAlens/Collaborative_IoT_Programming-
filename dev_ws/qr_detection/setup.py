01d04fa7b4fff67bcaa12c2c37eabc8f77e6eadc
Lukasdewachter <lukas.dewachter@student.kuleuven.be>
2025-03-20 11:57:22 +0100

    INIT

--- dev_ws/qr_detection/setup.py
+++ dev_ws/qr_detection/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-package_name = 'clamp'
+package_name = 'qr_detection'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -14,14 +14,12 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='student',
-    maintainer_email='student@todo.todo',
+    maintainer_email='lukas.dewachter@student.kuleuven.be',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'publisher = clamp.publisher:main',
-        'subscriber = clamp.subscriber:main'
         ],
     },
 )
