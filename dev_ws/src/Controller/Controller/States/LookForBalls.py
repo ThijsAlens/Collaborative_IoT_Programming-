ab03ad283b53f8ec9e70a9a9713825b7ec7dc075
student <joran.van.dolder@hotmail.com>
2025-03-29 16:36:17 +0100

    Added stop in WheelController. Added stopand start in states. Controller calls them. Added documentation

--- dev_ws/src/Controller/Controller/States/LookForBalls.py
+++ dev_ws/src/Controller/Controller/States/LookForBalls.py
@@ -10,27 +10,42 @@ class LookForBalls(State):
 
     """
     Looking for balls needs the ball inference controller and the wheels controller
+    params:
+        ballInferenceController: Controller for finding balls
+        wheelController: Controller for the wheels
     """
     def __init__(self, logger, ballInferenceController, wheelController):
         super().__init__("Looking for them balls", logger)
         self.ballInferenceController = ballInferenceController
         self.wheelController = wheelController
 
+    """
+    When the state starts, start rotating.
+    """
+    def start(self):
         self.logger.info("Rotating the wheels clockwise")
         self.wheelController.rotate_clockwise()
 
 
-
     """
-    The move command is in __init__()
-    Nothing to be done in Update.
+    The move command is in start()
+    Nothing to be done in update.
     """
     def update(self):
         """
         If the ball needs to be perfectly centered before moving on
-        Here is where corrective measures would take place
+        Here is where corrective measures would take place.
+        Or in a different state "CentreBall" if we're feeling fancy
         """
         pass
+
+    """
+    When this state finished, make sure to stop rotating the robot.
+    """
+    def stop(self):
+        self.logger.info("Stopping look for balls")
+        self.wheelController.stop()
+
 
     def isFinished(self):
         #Once a ball is found, this state can terminate
--- dev_ws/src/Controller/Controller/States/State.py
+++ dev_ws/src/Controller/Controller/States/State.py
@@ -6,20 +6,36 @@ Should be abstract, but I don't know python
 class State():
 
     """
-    When overriding add parameters for references to controllers
+    When overriding add parameters for references to controllers.
+    This is for configuring the state, not starting the robot
     params:
-        name - name of the state
-        logger - the logger from Node
+        name: name of the state
+        logger: the logger from Node
     """
     def __init__(self, name, logger):
         self.name = name
         self.logger = logger
 
     """
+    start is called when the state should commence. 
+    Here first orders should be given.
+    """
+    def start(self):
+        pass
+
+    """
     Controller has a timer which continuously calls this method.
     Override to have an update loop
     """
     def update(self):
+        pass
+
+
+    """
+    stop is called when transitioning away from the state.
+    The controllers should be reset to return to a default state.
+    """
+    def stop(self):
         pass
 
 
