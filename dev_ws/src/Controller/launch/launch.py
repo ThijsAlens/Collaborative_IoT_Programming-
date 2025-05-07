bf8c07bd919f6c47d48ff7e6a9249187a603eaf3
student <joran.van.dolder@hotmail.com>
2025-05-07 19:18:41 +0200

    Added clamp package, added launch.py

---
+++ dev_ws/src/Controller/launch/launch.py
@@ -0,0 +1,41 @@
+import launch
+import launch_ros.actions
+
+def generate_launch_description():
+
+    return launch.LaunchDescription([
+        
+        
+        launch_ros.actions.Node(
+            package = 'camera_read',
+            executable = 'ImagePublisher',
+            name = 'camera_read'
+        ),
+        launch_ros.actions.Node(
+            package = 'ball_inference',
+            executable = 'ball_inference',
+            name = 'ball_inference'
+        ),
+        launch_ros.actions.Node(
+            package = 'aruco_detection',
+            executable = 'detector',
+            name = 'aruco_decetion'
+        ), 
+        launch_ros.actions.Node(
+            package = 'wheels',
+            executable = 'WheelsPublisher',
+            name = 'wheels'
+        ),
+        launch_ros.actions.Node(
+            package = 'ultrasound',
+            executable = 'USPublisher',
+            name = 'ultrasound'
+        ),
+        launch_ros.actions.Node(
+            package = 'Controller',
+            executable = 'Controller',
+            name = 'Controller'
+        )
+    ])
+
+
